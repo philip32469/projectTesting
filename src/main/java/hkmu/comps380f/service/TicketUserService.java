ff993fa6406ab0f068c68061f150ff7c6a335251
cslkl <cslkl@Keith-Legion>
2022-04-03 15:27:23 +0800

    Lab 10 Task 1 completed

---
+++ src/main/java/hkmu/comps380f/service/TicketUserService.java
@@ -0,0 +1,36 @@
+package hkmu.comps380f.service;
+
+import hkmu.comps380f.dao.TicketUserRepository;
+import hkmu.comps380f.model.TicketUser;
+import hkmu.comps380f.model.UserRole;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.Resource;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TicketUserService implements UserDetailsService {
+
+    @Resource
+    TicketUserRepository ticketUserRepo;
+
+    @Override
+    public UserDetails loadUserByUsername(String username)
+            throws UsernameNotFoundException {
+        TicketUser ticketUser = ticketUserRepo.findById(username).orElse(null);
+        if (ticketUser == null) {
+            throw new UsernameNotFoundException("User '" + username + "' not found.");
+        }
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        for (UserRole role : ticketUser.getRoles()) {
+            authorities.add(new SimpleGrantedAuthority(role.getRole()));
+        }
+        return new User(ticketUser.getUsername(), ticketUser.getPassword(), authorities);
+    }
+}
